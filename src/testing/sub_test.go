1e832834b204ba83ca172c38dc8140be3697aabf
suntala <artichaut2023@gmail.com>
2025-02-04 06:04:07 +0100

    Move tests to demonstrate output writer behavior

--- src/testing/sub_test.go
+++ src/testing/sub_test.go
@@ -7,6 +7,8 @@ package testing
 import (
 	"bytes"
 	"fmt"
+	"log/slog"
+	"os"
 	"regexp"
 	"runtime"
 	"slices"
@@ -988,4 +990,66 @@ func TestNestedCleanup(t *T) {
 	if ranCleanup != 3 {
 		t.Errorf("unexpected cleanup count: got %d want 3", ranCleanup)
 	}
+}
+
+// =================== TEMPORARILY KEEPING T.OUTPUT TESTS TO DEMONSTRATE BEHAVIOR HERE ===================
+
+func TestCompareLogAndOutput(t *T) {
+	t.Log("first T.Log\nsecond T.Log")
+
+	fmt.Fprintln(t.provideOutputWriter(), "first T.Output\nsecond T.Output")
+
+	t.Error("error message")
+
+}
+
+func TestWriterUsingSlogHandler(t *T) {
+	// Slog log lines as they are currently printed out in tests.
+	logger1 := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{AddSource: true}))
+	logger1.Info("slog using standard output in parent test")
+
+	/*
+		t.Output() allows:
+		- the indentation of slog output to match t.Log() output
+		- printing of the output under the correct test header
+	*/
+	logger2 := slog.New(slog.NewTextHandler(t.provideOutputWriter(), nil))
+	logger2.Info("slog using t.Output in parent test")
+	t.Error("t.Log in parent test")
+
+	// Additionally, t.Output() indents slog output depending on the nesting level of the test.
+	t.Run("Subtest", func(t *T) {
+		logger3 := slog.New(slog.NewTextHandler(t.provideOutputWriter(), nil))
+		logger3.Info("slog using t.Output in subtest")
+		t.Error("t.Log in subtest")
+
+		// Without t.Output(), the slog log does not take into account the nesting level.
+		// This is in addition to the log being printed in the wrong section.
+		logger4 := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{AddSource: true}))
+		logger4.Info("slog using standard output in subtest")
+	})
+}
+
+func TestWriteShouldAwaitTrailingNewline(t *T) {
+	w := t.provideOutputWriter()
+
+	w.Write([]byte("Hel"))
+	w.Write([]byte("lo\nWorld\nInput to log\n\n\nMore logging\nShouldn't be logged"))
+	//TODO: If there was a newline after "also shouldn't be logged", the whole
+	// log might need to be indented 8 spaces.
+	w.Write([]byte("Also shouldn't be logged"))
+
+	t.Error("error message")
+}
+
+func TestShouldWriteEntireMultipleLineInputWhenSubtestIsDone(t *T) {
+	t.Run("Multiple lines from goroutine", func(t *T) {
+		go func() {
+			time.Sleep(50 * time.Millisecond)
+			w := t.provideOutputWriter()
+			w.Write([]byte("First line\nSecond line\nThird line\n"))
+		}()
+	})
+	time.Sleep(1 * time.Second)
+	t.Error("error message")
 }
